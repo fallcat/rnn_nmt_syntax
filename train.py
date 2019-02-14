5bf62c60d966e126a1ae65fe26e74d01918b9db0
Weiqiu You <rachel_you@outlook.com>
2019-02-14 10:50:02 -0500

    change training iterations to fit small data

--- train.py
+++ train.py
@@ -182,7 +182,7 @@ if __name__ == "__main__":
     encoder1 = EncoderRNN(len(vocab), args.hidden_size).to(device)
     attn_decoder1 = AttnKspanDecoderRNN(args.hidden_size, len(vocab), dropout_p=args.dropout).to(device)
 
-    train_iters(encoder1, attn_decoder1, 10000, num_layers=args.num_layers, print_every=5000, restore=args.restore)
+    train_iters(encoder1, attn_decoder1, 40000, num_layers=args.num_layers, print_every=5000, restore=args.restore)
     # trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
 
     start = time.time()
