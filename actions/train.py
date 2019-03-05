e95fe07c5efca371f49786c0c53b5a57c3771264
Weiqiu You <rachel_you@outlook.com>
2019-03-05 14:06:04 -0500

    debugging train batch

--- actions/train.py
+++ actions/train.py
@@ -53,7 +53,7 @@ class Trainer(object):
         decoder_input = Variable(torch.tensor([SOS_token] * self.config['span_size'], device=DEVICE))
         output_to_pad = [torch.cat((decoder_input, output_batch), 0) for output_batch in output_list]
         output_batches = torch.zeros((batch_size, self.config['max_length']), dtype=torch.long)
-        output_batches2 = torch.nn.utils.rnn.pad_sequence(output_to_pad)
+        output_batches2 = torch.nn.utils.rnn.pad_sequence(output_to_pad, batch_first=True)
         print("output_batches size", output_batches.size())
         print("output_batches2 size", output_batches2.size())
         output_batches[:, :output_batches2.size()[1]] += output_batches2
