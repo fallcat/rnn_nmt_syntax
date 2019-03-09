53e4c7925be976cda763724cce7cbe2a1e54b50e
Weiqiu You <rachel_you@outlook.com>
2019-03-09 15:27:34 -0500

    debug

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model.utils import get_cl_args
 from data.wmt import WMTDataset
 from actions.train import Trainer
 from actions.evaluate import Evaluator
-from model.seq2seq import EncoderRNN, AttnKspanDecoderRNN, BatchEncoderRNN, BatchAttnKspanDecoderRNN
+from model.seq2seq import EncoderRNN, AttnKspanDecoderRNN, BatchEncoderRNN, BatchAttnKspanDecoderRNN2
 from model import DEVICE
 
 # config: max_length, span_size, teacher_forcing_ratio, learning_rate, num_iters, print_every, plot_every, save_path,
@@ -19,8 +19,8 @@ def main():
     print(args)
     dataset = WMTDataset(max_length=args.max_length, span_size=args.span_size)
     encoder1 = BatchEncoderRNN(dataset.num_words, args.hidden_size, num_layers=args.num_layers).to(DEVICE)
-    attn_decoder1 = BatchAttnKspanDecoderRNN(args.hidden_size, dataset.num_words, num_layers=args.num_layers,
-                                        dropout_p=args.dropout).to(DEVICE)
+    attn_decoder1 = BatchAttnKspanDecoderRNN2(args.hidden_size, dataset.num_words, num_layers=args.num_layers,
+                                        dropout_p=args.dropout, max_length=args.max_length, span_size=args.span_size).to(DEVICE)
     models = {'encoder': encoder1, 'decoder': attn_decoder1}
     config = {
         'max_length': args.max_length,
