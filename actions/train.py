5a315ddf365f01ef90f6065420b64fdac8b39124
Weiqiu You <rachel_you@outlook.com>
2019-03-09 14:48:55 -0500

    debug

--- actions/train.py
+++ actions/train.py
@@ -80,6 +80,8 @@ class Trainer(object):
         # print("decoder_outputs", decoder_outputs.size())
         # print("output_batches", output_batches.size())
 
+        print("decoder_outputs[:, :-self.config['span_size']]", decoder_outputs[:, :-self.config['span_size']].size())
+        print("output_batches[:, self.config['span_size']:]", output_batches[:, self.config['span_size']:].size())
         loss += self.criterion(decoder_outputs[:, :-self.config['span_size']].view(-1, self.dataset.num_words),
                                output_batches[:, self.config['span_size']:].view(-1))
 
