ff82e8a0602419d5b196440bb39918134c61744d
Weiqiu You <rachel_you@outlook.com>
2019-04-03 14:48:23 -0400

    debug beam search

--- model/beam_search2.py
+++ model/beam_search2.py
@@ -105,7 +105,7 @@ class BeamSearchDecoder(object):
                             self.config['max_length'], self.config['beam_width'])
                 for l in range(int(self.config['max_length']/self.config['span_size'])):
                     sequences, scores, hiddens = beam.collate()
-                    decoder_output, decoder_hidden, decoder_attn = self.decoder(sequences[:, -self.config['span_size']:].to(device=DEVICE),
+                    decoder_output, decoder_hidden, decoder_attn = self.decoder(sequences[-self.config['span_size']:].to(device=DEVICE).unsqueeze(0),
                                                                                 hiddens.transpose(0, 1),
                                                                                 row[0])
                     topv, topi = decoder_output.topk(self.config['beam_width'], dim=2)
