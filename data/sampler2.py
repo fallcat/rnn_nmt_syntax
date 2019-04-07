a5407e791ae68e27674364a40abeba21c61a8fbb
Weiqiu You <rachel_you@outlook.com>
2019-04-07 19:16:02 -0400

    debug

--- data/sampler2.py
+++ data/sampler2.py
@@ -50,25 +50,13 @@ class SequenceLengthSampler(Sampler):
         self.shuffle = shuffle
 
         data_indices = [i[0] for i in sorted(enumerate(example_lengths), key=lambda x: x[1][1], reverse=True)]
-        # print("total indices", len(data_indices), len(example_lengths))
-        # print("data_indices", [example_lengths[i] for i in data_indices[:100]])
-
 
         i = 0
 
         while i < len(data_indices):
-            # print("i", i)
-            # print(len(self.batches))
             seq_len = example_lengths[data_indices[i]][1]
-            # print("batch_size", batch_size)
-            # print("seq_len", seq_len)
-            # print(example_lengths[data_indices[i]])
             batch_max_len = batch_size // seq_len
-            # print("batch_max_len", batch_max_len)
-            # print("NUM_DEVICES", NUM_DEVICES)
             batch_max_len -= batch_max_len % NUM_DEVICES
-            # print("batch_max_len", batch_max_len)
-            # print("batch_max_len", batch_max_len)
             self.batches.append(data_indices[i:i + batch_max_len])
             i += batch_max_len
 
@@ -87,6 +75,4 @@ class SequenceLengthSampler(Sampler):
             np.random.shuffle(batch_indices)
 
         for idx in batch_indices:
-            # print("idx", idx)
-            # print(len(self.batches[idx]))
             yield self.batches[idx]
