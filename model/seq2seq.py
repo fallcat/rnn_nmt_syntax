1cfaa8f120ce7959ae0fd88c2897456a9f216a85
Weiqiu You <rachel_you@outlook.com>
2019-04-16 10:55:15 -0400

    debug

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -102,6 +102,9 @@ class BatchAttnKspanDecoderRNN(nn.Module):
             rnn_output, hidden = self.gru(embeddeds, hidden)
         else:
             self.lstm.flatten_parameters()
+            print("embeddeds", embeddeds.size())
+            print("hidden", hidden.size())
+            print("cell", cell.size())
             rnn_output, (hidden, cell) = self.lstm(embeddeds, (hidden, cell))
 
         concatted = torch.cat((
