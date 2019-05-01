9bf5cf52e000912367c569c333003afa70315ad5
Weiqiu You <rachel_you@outlook.com>
2019-05-01 19:37:35 -0400

    debug

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -514,14 +514,14 @@ class BatchBahdanauAttnKspanDecoderRNN2(nn.Module):
         embeddeds = self.cat_embeddings(embeddeds)
         # print("embeddeds", embeddeds.size())
 
-        print("embeddeds", embeddeds.size())
-        print("hidden[-1]", hidden[-1].size())
+        # print("embeddeds", embeddeds.size())
+        # print("hidden[-1]", hidden[-1].size())
         attn_weights = F.softmax(
             self.attn(torch.cat((embeddeds, hidden[-1]), 1)), dim=1)
-        print("attn_weights", attn_weights.size())
+        # print("attn_weights", attn_weights.size())
         attn_applied = torch.bmm(attn_weights.unsqueeze(1),
                                  encoder_outputs)
-        print("attn_applied", attn_applied.size())
+        # print("attn_applied", attn_applied.size())
         embeddeds = torch.cat((embeddeds.unsqueeze(1), attn_applied), 2)
         embeddeds = self.attn_combine(embeddeds)
 
