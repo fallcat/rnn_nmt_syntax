7fc921b4142fb7785ded9a8b2646bf86caab0876
Weiqiu You <rachel_you@outlook.com>
2019-05-01 11:18:17 -0400

    use bahdanau attention

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main():
                                num_layers=args.num_layers,
                                rnn_type=args.rnn_type,
                                num_directions= args.num_directions).to(DEVICE)
-    attn_decoder1 = BatchAttnKspanDecoderRNNSmall(args.hidden_size,
+    attn_decoder1 = BatchBahdanauAttnKspanDecoderRNN(args.hidden_size,
                                              dataloader_train.dataset.num_words,
                                              num_layers=args.num_layers,
                                              dropout_p=args.dropout,
