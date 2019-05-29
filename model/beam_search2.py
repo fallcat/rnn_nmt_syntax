770f4cdaba9b00a30a4e2d9d078a0aa6f57e4be6
Weiqiu You <rachel_you@outlook.com>
2019-05-29 17:02:10 -0400

    debug beam search to use normalize score best

--- model/beam_search2.py
+++ model/beam_search2.py
@@ -132,7 +132,7 @@ class BeamSearchDecoder(object):
                                          for col in row] for row in b_matrix_list], dtype=torch.float32)
                 print("newscores", newscores.size())
                 print("lengths", lengths.size())
-                newscores = self.normalized_score(newscores.view(batch_size, self.config['beam_width'], -1), lengths - self.config['span_size'])
+                newscores = self.normalized_score(newscores, lengths - self.config['span_size'])
 
             topsv, topsi = newscores.view(batch_size, -1).topk(self.config['beam_width'], 1)
             rowsi = (topsi // self.config['beam_width'])  # indices of the topk beams
