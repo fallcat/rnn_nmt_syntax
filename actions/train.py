78bbec9767b8786e4666024a75a4ea437042b651
Weiqiu You <rachel_you@outlook.com>
2019-06-03 12:48:19 -0400

    multiply the number to decrease by the accumulate steps

--- actions/train.py
+++ actions/train.py
@@ -53,7 +53,7 @@ class Trainer(object):
         else:
             self.lr_scheduler = optim.lr_scheduler.LambdaLR(
                 self.optimizer,
-                [lambda step: 1 - step * config['lr_decay']]
+                [lambda step: 1 - step * config['lr_decay'] * config['accumulate_steps']]
             )
         self.criterion = Parallel(
             LabelSmoothingLoss(
